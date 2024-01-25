8c92a82458a03fdcc1749a4415d373570ccdc2ba
Peter <97176773+PeterZhang26@users.noreply.github.com>
2024-01-25 16:24:00 +0200

    Exercise 109 - List Check

---
+++ exercise_109-list_check.py
@@ -0,0 +1,16 @@
+# Write a function called list_check, which accepts a list and returns True if each value in the list is a list. Otherwise the function should return False.
+
+'''
+list_check([[],[1],[2,3], (1,2)]) # False
+list_check([1, True, [],[1],[2,3]]) # False
+list_check([[],[1],[2,3]]) # True
+'''
+
+def list_check(my_list):
+    for val in my_list:
+        if type(val) != list:
+            return False
+        
+    return True
+
+print(list_check([[],[1],[2,3]]))
