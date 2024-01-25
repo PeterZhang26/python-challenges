8467e3700cd90c9ab17717ac06d387c774d9d6b8
Peter <97176773+PeterZhang26@users.noreply.github.com>
2024-01-25 16:19:44 +0200

    Exercise 108 - Reverse string

---
+++ exercise_108-reverse_string.py
@@ -0,0 +1,16 @@
+# Write a function called reverse_string which accepts a string and returns a new string with all the characters reversed.
+
+"""
+reverse_string('awesome') # 'emosewa'
+reverse_string('Colt') # 'tloC'
+reverse_string('Elie') # 'eilE'
+"""
+
+
+# add whatever parameters you deem necessary - good luck!
+def reverse_string(text):
+    # implement your function here
+    return text[::-1]
+
+
+print(reverse_string("Hello my name is Peter"))
