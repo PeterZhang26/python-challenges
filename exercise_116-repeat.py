5e6a8f2b03fe9565f1c052fc7f78406a70c7e7ef
Peter <97176773+PeterZhang26@users.noreply.github.com>
2024-02-05 22:00:24 +0200

    Exercise 116 - Repeat

---
+++ exercise_116-repeat.py
@@ -0,0 +1,17 @@
+# Write a function called repeat, which accepts a string and a number and returns a new string with the string passed to the function repeated the number amount of times. Do not use the built in repeat method!
+
+"""
+repeat('*', 3) # '***' 
+repeat('abc', 2) # 'abcabc' 
+repeat('abc', 0) # ''
+"""
+
+
+def repeat(string, num):
+    if num == 0:
+        return ""
+    return string * num
+
+
+print(repeat("rockstar", 3))
+print(repeat("empty", 0))
