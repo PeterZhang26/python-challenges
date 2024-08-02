36fe0059469bd342f37a131da2d9b7e294a03369
Peter <97176773+PeterZhang26@users.noreply.github.com>
2024-08-02 13:34:16 +0200

    Added docstring explanation ex 129

--- exercise_129-reverse_vowels.py
+++ exercise_129-reverse_vowels.py
@@ -39,3 +39,15 @@ print(reverse_vowels("Tomatoes"))  # "Temotaos"
 print(reverse_vowels("Reverse Vowels In A String"))  # "RivArsI Vewols en e Streng"
 print(reverse_vowels("aeiou"))  # "uoiea"
 print(reverse_vowels("why try, shy fly?"))  # "why try, shy fly?"
+
+"""
+Within the function itself we first create a string variable with lowercase vowels,
+then we convert the string passed into the function into a list. We initialize two 
+iterators, i and j that loop from the start and the end of the string at the same time.
+If the iterator i is not a vowel, we move to the right, if the iterator at j is not a
+vowel we move left. We only swap the vowels when both i and j iterators are a vowel.
+The iterator j starts at len(s) - 1 because of the way python indexing works, the word
+"Hello!" has an index of 0 to 5, but the length of the string is 6 so we want to start
+at index 5. Once the vowels are swapped, we return the an empty string with the method
+.join on the string that we've swapped the vowels around.
+"""
